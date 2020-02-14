4ae0e473e72186406cb7f0d11ae93a0a40355d44
grepix <grepixrepoall@gmail.com>
2020-02-14 18:37:05 +0530

    14-02-20

--- app/src/main/java/com/my/musicplayer/MainActivity.java
+++ app/src/main/java/com/my/musicplayer/MainActivity.java
@@ -5,22 +5,31 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.annotation.SuppressLint;
+import android.content.Context;
+import android.media.AudioManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
+import android.widget.ImageView;
 import android.widget.RelativeLayout;
+import android.widget.SeekBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.bottomsheet.BottomSheetBehavior;
 
-public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener, View.OnClickListener {
     private RelativeLayout smallSizeMusicController;
     private ConstraintLayout fullSizeMusicController;
     private FrameLayout standardBottomSheet;
+    private BottomSheetBehavior bottomSheetBehavior;
+    private TextView song_name;
+    private SeekBar volumeSeekbar = null;
+    private AudioManager audioManager = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,14 +40,14 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
         standardBottomSheet = findViewById(R.id.standardBottomSheet);
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
         bottomNavigationView.setOnNavigationItemSelectedListener(this);
-        final BottomSheetBehavior bottomSheetBehavior = BottomSheetBehavior.from(findViewById(R.id.standardBottomSheet));
-
+        bottomSheetBehavior = BottomSheetBehavior.from(findViewById(R.id.standardBottomSheet));
+        bottomSheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
+        bottomSheetBehavior.setHideable(false);
         bottomSheetBehavior.setBottomSheetCallback(new BottomSheetBehavior.BottomSheetCallback() {
             @Override
             public void onStateChanged(@NonNull View bottomSheet, int newState) {
                 if (newState == BottomSheetBehavior.STATE_EXPANDED) {
                     smallSizeMusicController.setVisibility(View.GONE);
-                    fullSizeMusicController.setVisibility(View.VISIBLE);
                     fullSizeMusicController.setBackgroundResource(R.drawable.bottom_sheet_corner);
 
                 } else if (newState == BottomSheetBehavior.STATE_COLLAPSED) {
@@ -51,13 +60,14 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
 
                 }
             }
+
             @Override
             public void onSlide(@NonNull View bottomSheet, float slideOffset) {
                 bottomSheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED);
 
             }
         });
-
+        initControls();
     }
 
     @Override
@@ -77,5 +87,41 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
                 break;
         }
         return true;
+    }
+
+    @Override
+    public void onClick(View v) {
+        // if (v.getId()== R.id.song_name)
+        //bottomSheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED);
+    }
+
+    private void initControls() {
+        try {
+            volumeSeekbar = findViewById(R.id.volumeBar);
+            audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+            volumeSeekbar.setMax(audioManager
+                    .getStreamMaxVolume(AudioManager.STREAM_MUSIC));
+            volumeSeekbar.setProgress(audioManager
+                    .getStreamVolume(AudioManager.STREAM_MUSIC));
+
+            volumeSeekbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+                @Override
+                public void onStopTrackingTouch(SeekBar arg0) {
+                }
+
+                @Override
+                public void onStartTrackingTouch(SeekBar arg0) {
+                }
+
+                @Override
+                public void onProgressChanged(SeekBar arg0, int progress, boolean arg2) {
+                    audioManager.setStreamVolume(AudioManager.STREAM_MUSIC,
+                            progress, 0);
+
+                }
+            });
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
